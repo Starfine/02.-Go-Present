d726ac05f4b9d77d4ea81d9d5f18a4e14b56dd1c
Jing-Siang Chen <cookie@Jing-Siangde-MacBook-Air.local>
2017-01-27 23:44:50 +0800

    First commit

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+//show A type OMIT
+type A struct {
+	name string
+}
+
+//end show A type OMIT
+
+//start main OMIT
+func main() {
+	fmt.Println("hello world. sugeladi")
+}
+
+//end main OMIT
+
+func printStr(s string) {
+	fmt.Println(s) // HL
+}
